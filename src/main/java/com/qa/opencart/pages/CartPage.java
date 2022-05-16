acf375962450cdcc6dc14fd857a211a9580a3f76
nagalakshmi-github <nagalakshmi2329@gmail.com>
2022-05-16 15:28:39 +0530

    added changes to cart page based on Naveen review comments

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,9 +1,13 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 	
+	private By cartBtn=By.id("cart");
+	
 	public void getCart() {
-		System.out.println("get Cart");
+		System.out.println("get Cart: "+cartBtn);
 	}
 
 }
